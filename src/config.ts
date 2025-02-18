2a457d1336a2de8a8ac91770ce3a8611c60f5ffb
Hwantae Kim <khtinsoft@gmail.com>
2025-02-18 18:26:27 +0900

    chore: update version

--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,10 @@ import { Connection, DynamoDBConnection } from "./connections";
 export default class Config {
   private static __defaultConnection: Connection; // tslint:disable-line
   public static get defaultConnection() {
+
     if (!this.__defaultConnection) {
       this.__defaultConnection = new DynamoDBConnection({
+        region: (process.env.DYNAMO_TYPES_REGION as string | undefined) || undefined,
         endpoint: process.env.DYNAMO_TYPES_ENDPOINT as string | undefined,
         enableAWSXray: process.env.ENABLE_XRAY === "true",
       });
